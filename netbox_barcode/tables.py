33bc0ff37cab803dbd641edeed895c32dfd6dfbd
aakozlov85 <aakozlov85@yandex.ru>
2023-07-20 15:19:05 +0300

    добавлена нумерация строк в таблице

--- netbox_barcode/tables.py
+++ netbox_barcode/tables.py
@@ -1,18 +1,27 @@
 import django_tables2 as tables
 from netbox.tables import NetBoxTable
 from dcim.models import Device
+import itertools
 
 
 class BarcodeTable(NetBoxTable):
+    counter = tables.Column(verbose_name='#', empty_values=(), orderable=False)
     name = tables.Column(
-        linkify=True
+        linkify=True,
+        orderable=False
     )
-    serial = tables.Column()
-    device_type = tables.Column()
-    partnumber = tables.Column(accessor='device_type.part_number')
+    serial = tables.Column(orderable=False)
+    device_type = tables.Column(orderable=False)
+    partnumber = tables.Column(accessor='device_type.part_number',
+                               orderable=False
+                               )
     actions = tables.TemplateColumn(verbose_name='Action',
                                     template_name='netbox_barcode/table_actions.html', orderable=False)
 
     class Meta(NetBoxTable.Meta):
         model = Device
-        fields = ('name', 'serial', 'device_type', 'partnumber')
+        fields = ('counter', 'name', 'serial', 'device_type', 'partnumber')
+
+    def render_counter(self):
+        self.row_counter = getattr(self, 'row_counter', itertools.count())
+        return next(self.row_counter)+1
